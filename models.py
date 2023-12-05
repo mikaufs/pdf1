a2868534089779a4956575a6dfe4b4c58a42576c
Murilo <61352608+mikaufs@users.noreply.github.com>
2023-12-05 14:25:29 -0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=20)
+    endereco = models.CharField(max_length=255)
+
+class Quarto(models.Model):
+    apartamento = models.IntegerField()
+    valor = models.FloatField()
+
+class Hospedagem(models.Model):
+    data_entrada = models.DateField()
+    data_saida = models.DateField()
+    valor = models.FloatField()
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    quarto = models.ForeignKey(Quarto, on_delete=models.CASCADE)
+
+class Consumo(models.Model):
+    nome = models.CharField(max_length=255)
+    data = models.DateField()
+    valor = models.FloatField()
+    hospedagem = models.ForeignKey(Hospedagem, on_delete=models.CASCADE)
